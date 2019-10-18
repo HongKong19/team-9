b69a67adcfa15f48117d9b97b8d4a0a2feb78192
KwanyoungLee <u3534739@connect.hku.hk>
2019-10-18 14:31:06 +0800

    login without nabbar

--- src/App.js
+++ src/App.js
@@ -2,26 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import  "./views/login";
-import Login from './views/login';
+import SignIn from './views/login';
 
 function App() {
   return (
     <div className="App">
     <login/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Login name="Magow"/>
+
+        <SignIn/>
       </header>
     </div>
   );
